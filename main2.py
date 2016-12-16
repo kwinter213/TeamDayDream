70c9edc2e3cb018385b93b9b9d7319b300e2956a
Kimber <kimberly.winter@students.olin.edu>
2016-12-15 23:54:56 -0500

    newest version of code

--- main2.py
+++ main2.py
@@ -21,8 +21,15 @@ overlay_starttime = 0
 overlay_playing = False
 stableTime=50 #iterations before average is taken-- used for stabilization
 
-blackLower=numpy.array([0,0,0]) #black
 blackUpper=numpy.array([50, 50, 50])
+blackLower=numpy.array([0,0,0]) #black
+#black=numpy.array([0,0,0],[50,50,50]) #defining black
+
+orangeLower=numpy.array([5, 50, 150]) 
+orangeUpper=numpy.array([100, 200, 255]) #represents upper and lower bounds of the color "orange"
+
+colors=([blackLower,blackUpper],[orangeLower,orangeUpper])
+print colors[0][0]
 
 def loadOverlayVideo():
     """
@@ -53,15 +60,11 @@ def overlayFrame(frame):
     for c in range(0,3):
         alpha = video_overlay[frame_no][:,:, 3] / 255.0
         color = video_overlay[frame_no][:,:, c] * (alpha)
-        print centre_x
-        print "centre_x"
-        print centre_y
-        print "centre_y"
         beta  = frame[centre_y : centre_y + overlay_h, centre_x : centre_x + overlay_w, c] * (1.0 - alpha)
         frame[centre_y : centre_y + overlay_h, centre_x : centre_x + overlay_w, c] = color + beta
 
-def largeRectangle(frame): #finds the largest rectangle created in a frame by a black object
-    mask=cv2.inRange(frame, blackLower,blackUpper) #creates mask of all the pixels
+def largeRectangle(frame,c): #finds the largest rectangle created in a frame by a black object, 1st input is image, 2nd input is color that is being searched for
+    mask=cv2.inRange(frame, c[0], c[1]) #creates mask of all the pixels
     output=cv2.bitwise_and(frame,frame,mask=mask) #maps mask onto image
 
 #getting rid of false negatives and other outliers/smoothing over larger objects
@@ -121,41 +124,43 @@ while(True):
     ret, frame = cap.read() #gets the frame
     #orangeLower=numpy.array([5, 50, 150], dtype="uint8") #uint8 necessary for this kind of thing
     #orangeUpper=numpy.array([100, 200, 255], dtype= "uint8") #represents upper and lower bounds of the color "orange"
-    minx, maxx, miny, maxy, output=largeRectangle(frame)
-    if count<stableTime:
-        minxsum+=minx
-        minysum+=miny
-        maxxsum+=maxx
-        maxysum+=maxy
-        count+=1
-    elif count==stableTime:
-        #taking averages
-        minxAverage=minxsum/stableTime
-        minyAverage=minysum/stableTime
-        maxxAverage=maxxsum/stableTime
-        maxyAverage=maxysum/stableTime
+    for c in colors: #iterating through each color
+        minx, maxx, miny, maxy, output=largeRectangle(frame, c)
+        if(isinstance( <minx, int ))
+            if count<stableTime:
+                minxsum+=minx
+                minysum+=miny
+                maxxsum+=maxx
+                maxysum+=maxy
+                count+=1
+            elif count==stableTime:
+                #taking averages
+                minxAverage=minxsum/stableTime
+                minyAverage=minysum/stableTime
+                maxxAverage=maxxsum/stableTime
+                maxyAverage=maxysum/stableTime
 
-        #resetting values
-        minxsum=0
-        maxxsum=0
-        minysum=0
-        maxysum=0
-        count=0
+                #resetting values
+                minxsum=0
+                maxxsum=0
+                minysum=0
+                maxysum=0
+                count=0
 
-        #the actual rectangle calculation
-        cv2.rectangle(output, (minxAverage,minyAverage),(maxxAverage-minxAverage,maxyAverage-minyAverage),(0,255,0),2)
-        centre_x = (maxxAverage + minxAverage)/2.0
-        centre_y = (maxyAverage + minyAverage)/2.0
-        print [centre_x, centre_y]
-        playoverlay(centre_x - overlay_w / 2, centre_y - overlay_h / 2)
-    #rect = cv2.minAreaRect(contours)
-    #box = cv2.boxPoints(rect)
-    #box = np.int0(box)
-    #cv2.drawContours(img,[box],0,(0,0,255),2)
-    #ret, frame = cap.read()
-    # Display the resulting frame
-    if overlay_playing:
-        overlayFrame(frame)
+                #the actual rectangle calculation
+                cv2.rectangle(output, (minxAverage,minyAverage),(maxxAverage-minxAverage,maxyAverage-minyAverage),(0,255,0),2)
+                centre_x = (maxxAverage + minxAverage)/2.0
+                centre_y = (maxyAverage + minyAverage)/2.0
+                print [centre_x, centre_y]
+                playoverlay(centre_x - overlay_w / 2, centre_y - overlay_h / 2)
+            #rect = cv2.minAreaRect(contours)
+            #box = cv2.boxPoints(rect)
+            #box = np.int0(box)
+            #cv2.drawContours(img,[box],0,(0,0,255),2)
+            #ret, frame = cap.read()
+            # Display the resulting frame
+            if overlay_playing:
+                overlayFrame(frame)
 ###Detection part
 
 
